4c5a49debd68604ecf78389cb5d54eb573d237ed
obyparaiso <obinnaikechukwu1958@gmail.com>
2022-09-09 17:10:56 -0700

    Adding exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+	{
+		if (a == 'e' || a == 'q')
+		{
+			continue;
+		}
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
