7b24fa97cc891f7c12c88c3992203bf401786798
obyparaiso <obinnaikechukwu1958@gmail.com>
2022-09-14 03:46:42 -0700

    Addition is cool

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *add - a function that adds two integers
+ *@i:integer to be added
+ *@c: second integer to be added
+ *
+ *Return: the result.
+ */
+int add(int i, int c);
+{
+	int i, c;
+
+	int add = i + c
+		for (int add)
+		{
+			return (add);
+		}
+}
