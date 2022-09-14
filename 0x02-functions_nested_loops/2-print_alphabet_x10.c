2585168e466c0c104c61035ad93fafab52d11875
obyparaiso <obinnaikechukwu1958@gmail.com>
2022-09-13 18:35:56 -0700

    Count alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_alphabeth_x10 - a function to print alphabeth x10
+ * print_alphabet_x10 - a function that prints 10 times the alphabet, lowercase
  *
- * Return: 0 (Success)
+ * return: 0 (Success)
  */
 void print_alphabet_x10(void)
 {
