4b7a37da793a7ffafef6910a7e44274c84b904fa
obyparaiso <obinnaikechukwu1958@gmail.com>
2022-09-13 19:49:04 -0700

    Absolute path

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - a function that computes the absolute value of an integer
+ * @int: is the int that will use the argument of the function
+ * Return: 0 (Success)
+ */
+int _abs(int);
+{
+	if (c < 0 || c == 0)
+	{
+		return (c);
+	}
+	else
+	return (c * -1);
+}
