d5e6f7a2a4262fdfbe6f743b3daafad21b5898b6
obyparaiso <obinnaikechukwu1958@gmail.com>
2022-09-14 02:47:06 -0700

    Minutes of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,26 +5,23 @@
  * Return: 0
  */
 void jack_bauer(void)
-{q
-	int hours = 0;
-	int minutes = 0;
-	int hours_remainder;
-	int mins_remainder;
+{
+	int hours;
+	int minutes;
 
-	while (hours <= 23)
+	int hours = h;
+	int minutes = m;
+
+	for (h = 0; h <= 23; h++)
 	{
-		while (minutes <= 59)
+		for (j = 0; j <= 59; j++)
 		{
-			mins_remainder = minutes % 10;
-			hours_remainder = hours % 10;
-			_putchar(hours / 10 + '0');
-			_putchar(hours_remainder + '0');
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
 			_putchar(':');
-			_putchar(minutes / 10 + '0');
-			_putchar(mins_remainder + '0');
-			minutes++;
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 			_putchar('\n');
 		}
 	}
-	hours++;
-	minutes = 0;
+}
