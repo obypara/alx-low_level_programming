621121a50a007f47eda8156209a41174721e3d76
obyparaiso <obinnaikechukwu1958@gmail.com>
2022-09-15 09:55:04 -0700

    Multipication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - a function that multiplies two int
+ *
+ * @a: the int to be multipied
+ * @b: the int to be multipied
+ *
+ * Return: result of the two
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
