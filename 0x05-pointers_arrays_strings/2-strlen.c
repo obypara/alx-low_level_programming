2fb9117d3227f0b2110c957fdf2e74de1855e610
obyparaiso <obinnaikechukwu1958@gmail.com>
2022-09-19 14:06:19 -0700

    Length of char

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - return string length
+ *
+ * @s: string parameter
+ *
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
