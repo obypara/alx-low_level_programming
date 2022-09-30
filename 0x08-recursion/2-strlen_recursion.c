92ba9f4414bb2f1924dd9d73cffd86f909d7b55b
obyparaiso <obinnaikechukwu1958@gmail.com>
2022-09-29 17:56:33 -0700

    String length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,10 +3,11 @@
 /**
  * _strlen_recursion - returms tge length of string
  * @s: string to print
+ * Return: length of string
  */
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-	return (0);
+	if (*s == '\0')
+		return (0);
 	return (1 + _strlen_recursion(++s));
 }
